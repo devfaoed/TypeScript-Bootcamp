99bf3abb5e8ebd75118caa2c1585d1f9b7d82d65
devfaoed <faithadedokun2018@gmail.com>
2023-05-25 14:58:01 -0700

    first learning stage

---
+++ intro/lib/index.js
@@ -0,0 +1,5 @@
+"use strict";
+let message = "hello world welcome to my typescript code";
+let times = 200;
+message += " hope too learn a lot from here " + times + " times again and again";
+console.log(message);
---
+++ intro/src/index.ts
@@ -0,0 +1,4 @@
+let message: string = "hello world welcome to my typescript code";
+let times: number = 200
+message += " hope too learn a lot from here " + times + " times again and again"
+console.log(message)
