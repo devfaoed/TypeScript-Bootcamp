d904423001e6cb5005ab44524e0c865870e343ee
devfaoed <faithadedokun2018@gmail.com>
2023-06-01 08:44:35 -0700

    ahead

--- project_1/src/routes/api/imageRoute.ts
+++ project_1/src/routes/api/imageRoute.ts
@@ -1,10 +1,26 @@
 import express from "express";
+import sharp from "sharp";
 
 const imageRoute = express.Router();
 
+// geta all images
 imageRoute.get("/images", async (req, res) => {
   try {
     res.status(200).send("this route display all image");
+  } catch (err) {
+    err;
+  }
+});
+
+// get a specific image and reseize
+imageRoute.get("/image/reseize", async (req, res) => {
+  try {
+    const reseize = await sharp("../asset/img/full/hero-cover-01.jpg")
+      .rotate()
+      .resize(200, 200)
+      .toFile("../asset/img/reseize/hero-cover-01.jpg");
+    res.status(200).send(reseize);
+    console.log("image processed successfully");
   } catch (err) {
     err;
   }
